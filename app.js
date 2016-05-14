ce95c6a80bae202e26786f74ece9ddeae0e5bad5
Affixus_Shashikant <shashikantyadav@affixus.com>
2016-05-14 17:42:26 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Affixus_Shashikant on 5/14/2016.
+ */
+var express=require("express");
+var app=express();
+
+app.listen(4000,function () {
+    console.log("server started");
+    
+});
