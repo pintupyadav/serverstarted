195eee4ee0a8f563d3190060d5c84ef894870b75
Affixus_Shashikant <shashikantyadav@affixus.com>
2016-05-14 18:48:02 +0530

    app1

--- app1.js
+++ app1.js
@@ -6,12 +6,33 @@ var express=require("express");
 var app=express();
 
 app.get("/dc",function (req,res) {
-    res.send("API 2 For GET");
+    var query = req.query;
+    var name = query.name;
+
+    res.send("API 2 For GET :: " + name);
 
 });
-app.post("/dc",function (req,res) {
-    res.send("API 2 For POST")
 
+app.post("/dc",function (req,res) {
+    var query = req.query;
+    var name = query.name;
+    var email=query.email;
+    res.send("API 2 For POST :: " + name+" Email:: "+email);
+
+});
+app.get("/user",function (req,res) {
+    var query=req.query;
+    var name=query.name;
+     res.send("API 2 for get user"+name);
+
+});
+
+app.post("/user",function (req,res) {
+    var query=req.query;
+    var name=query.name;
+    var email=query.email;
+    res.send("API 2 for post user ::" +
+        "                   "+name+"::"+email);
 
 });
 
